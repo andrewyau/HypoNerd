a5253a84a98398ac034e6f2a9597afd795c19f8e
Andrew Yau <yauandrew@Andrew-Yaus-iMac.local>
2015-08-31 23:31:51 +0100

    Initial Commit

---
+++ HypoNerd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HypoNerd
+//
+//  Created by Andrew Yau on 31/08/2015.
+//  Copyright (c) 2015 Andrew Yau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HypoNerd/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HypoNerd
+//
+//  Created by Andrew Yau on 31/08/2015.
+//  Copyright (c) 2015 Andrew Yau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
